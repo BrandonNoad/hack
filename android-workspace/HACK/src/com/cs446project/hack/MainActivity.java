4b0d1bb76a916301003ee15022233a5471cbb492
Andrew K <alexisonfire@gmail.com>
2014-02-11 23:44:54 -0500

    Added check for Bluetooth availability and so far works on BB10.

--- android-workspace/HACK/src/com/cs446project/hack/MainActivity.java
+++ android-workspace/HACK/src/com/cs446project/hack/MainActivity.java
@@ -3,15 +3,30 @@ package com.cs446project.hack;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.bluetooth.*;
+import android.content.res.Resources;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Create things defined in activity_main
         setContentView(R.layout.activity_main);
+        m_t = (TextView)findViewById(R.id.msg);
+        
+        // Try and access bluetooth
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device does not support Bluetooth
+        	m_t.setText(R.string.msg_noBT);
+        } else {
+        	m_t.setText(R.string.msg_yesBT);
+        }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +34,10 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+ 
+    public void test(View view) {
+
+    }
     
+    private TextView m_t;
 }

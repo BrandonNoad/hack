85cb5a820e4ec3961b315e1c4ca9ceb0a6126bd7
Brandon Noad <Brandon.Noad@gmail.com>
2014-03-22 09:41:47 -0400

    Added new classes to match design doc

---
+++ android/src/com/hack/HackBluetoothAdapter.java
@@ -0,0 +1,5 @@
+package com.hack;
+
+public class HackBluetoothAdapter extends HackConnectionAdapter {
+
+}
---
+++ android/src/com/hack/HackConnectionAdapter.java
@@ -0,0 +1,9 @@
+package com.hack;
+
+public class HackConnectionAdapter {
+    
+    public void submitRequest(HackCommand command) {
+        
+    }
+
+}
